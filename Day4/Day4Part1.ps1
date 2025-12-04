0d998032d2090056a7762a763f0f815296c4e211
emilyLedesma <emilyledesma44@gmail.com>
2025-12-04 08:14:55 -0600

    Completing part 1

---
+++ Day4/Day4Part1.ps1
@@ -0,0 +1,96 @@
+#Goal is to get the sum of the max joltage of the rows
+#Get the sum of the max 2 digit number in the order they appear in the row for each row
+#Setting the input path to the input file 
+$InputPath = $PSScriptRoot + '.\Input.txt'
+
+#Variable to store the amount of accessible rolls
+$Accessible = 0
+
+#Parsing the input into the row list
+$Input = Get-Content -Path $InputPath
+$Diagram = [string[]]$Input -split ' '
+
+For ($Row = 0; $Row -lt $Diagram.Length; $Row++) {
+    $CurrentRow = $Diagram[$Row]
+
+    For ($Col = 0; $Col -lt $CurrentRow.Length; $Col++) {
+        $Adjacent = 0
+        $Cell = $Diagram[$Row][$Col]
+
+        If ($Cell -ne '@') {
+            continue
+        }
+
+        #Check for corners (Only has 3 adjacent cells)
+        If (($Row -eq 0 -and $Col -eq 0) -or ($Row -eq $Diagram.Length - 1 -and $Col -eq $CurrentRow.Length - 1)) {
+            $Accessible = $Accessible + 1
+            continue
+        }
+
+        If ($Row -ne 0) {
+            $AdjacentCell = $Diagram[$Row - 1][$Col]
+
+            If ($AdjacentCell -eq '@') {
+                $Adjacent = $Adjacent + 1
+            }
+        } 
+        If ($Row -ne $Diagram.Length - 1) {
+            $AdjacentCell = $Diagram[$Row + 1][$Col]
+
+            If ($AdjacentCell -eq '@') {
+                $Adjacent = $Adjacent + 1
+            }
+        } 
+
+        If ($Col -ne 0) {
+            $AdjacentCell = $Diagram[$Row][$Col - 1]
+
+            If ($AdjacentCell -eq '@') {
+                $Adjacent = $Adjacent + 1
+            }
+        }
+        If ($Col -ne $CurrentRow.Length - 1) {
+            $AdjacentCell = $Diagram[$Row][$Col + 1]
+
+            If ($AdjacentCell -eq '@') {
+                $Adjacent = $Adjacent + 1
+            }
+        }
+
+        If ($Row -ne 0 -and $Col -ne 0) {
+            $AdjacentCell = $Diagram[$Row - 1][$Col - 1]
+
+            If ($AdjacentCell -eq '@') {
+                $Adjacent = $Adjacent + 1
+            }
+        } 
+        If ($Row -ne $Diagram.Length - 1 -and $Col -ne 0) {
+            $AdjacentCell = $Diagram[$Row + 1][$Col - 1]
+
+            If ($AdjacentCell -eq '@') {
+                $Adjacent = $Adjacent + 1
+            }
+        } 
+        If ($Row -ne 0 -and $Col -ne $CurrentRow.Length - 1) {
+            $AdjacentCell = $Diagram[$Row - 1][$Col + 1]
+
+            If ($AdjacentCell -eq '@') {
+                $Adjacent = $Adjacent + 1
+            }
+        } 
+        If ($Row -ne $Diagram.Length - 1 -and $Col -ne $CurrentRow.Length - 1) {
+            $AdjacentCell = $Diagram[$Row + 1][$Col + 1]
+
+            If ($AdjacentCell -eq '@') {
+                $Adjacent = $Adjacent + 1
+            }
+        } 
+
+        #Write-Host $Adjacent $Row,$Col
+        If ($Adjacent -lt 4) {
+            $Accessible = $Accessible + 1
+        }
+    }
+}
+
+Write-Host $Accessible
