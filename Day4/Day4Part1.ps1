8e90f55f315c4d58480247b483949227655e3b4b
Emily Ledesma <130182539+emilyLedesma@users.noreply.github.com>
2025-12-04 14:49:17 -0600

    Adding comments

--- Day4/Day4Part1.ps1
+++ Day4/Day4Part1.ps1
@@ -1,22 +1,25 @@
-#Goal is to get the sum of the max joltage of the rows
-#Get the sum of the max 2 digit number in the order they appear in the row for each row
+#Goal is to get the amount of accessible rolls
+#Remove accessible rolls and keep iterating until there are no more accessible, get the total
 #Setting the input path to the input file 
 $InputPath = $PSScriptRoot + '.\Input.txt'
 
-#Variable to store the amount of accessible rolls
+#Variable to store the amount of accessible rolls and if there are any
 $Accessible = 0
 
 #Parsing the input into the row list
 $Input = Get-Content -Path $InputPath
 $Diagram = [string[]]$Input -split ' '
 
+#Go through each row in the diagram
 For ($Row = 0; $Row -lt $Diagram.Length; $Row++) {
     $CurrentRow = $Diagram[$Row]
 
+    #Go through each cell
     For ($Col = 0; $Col -lt $CurrentRow.Length; $Col++) {
         $Adjacent = 0
         $Cell = $Diagram[$Row][$Col]
 
+        #Skip if not a roll
         If ($Cell -ne '@') {
             continue
         }
@@ -27,6 +30,7 @@ For ($Row = 0; $Row -lt $Diagram.Length; $Row++) {
             continue
         }
 
+        #Check the cells on the sides
         If ($Row -ne 0) {
             If ($Diagram[$Row - 1][$Col] -eq '@') {
                 $Adjacent = $Adjacent + 1
@@ -49,6 +53,7 @@ For ($Row = 0; $Row -lt $Diagram.Length; $Row++) {
             }
         }
 
+        #Check the cells diagonal to it
         If ($Row -ne 0 -and $Col -ne 0) {
             If ($Diagram[$Row - 1][$Col - 1] -eq '@') {
                 $Adjacent = $Adjacent + 1
@@ -70,11 +75,12 @@ For ($Row = 0; $Row -lt $Diagram.Length; $Row++) {
             }
         } 
 
-        #Write-Host $Adjacent $Row,$Col
+        #Verify if it is accessible or not, accessible rolls will have less than 4 adjacent to it
         If ($Adjacent -lt 4) {
             $Accessible = $Accessible + 1
         }
     }
 }
 
-Write-Host $Accessible
+#Get the total amount accessible
+Write-Host $Accessible #1518
